fd074910e305a15dbe2513eacec37517c105f6d9
tsalmon <th_s@hotmail.fr>
2014-10-27 17:54:37 +0100

    tidytree class : add() ; rm other methods

--- src/tidytree.py
+++ src/tidytree.py
@@ -14,50 +14,24 @@ def getListofFiles(folder):
 
 class TidyTree:
 
-    def __init__(self, files, folder, letter=None):
-        self.files = files
-        self.letter = letter
-        self.limit = 5
+    def __init__(self, letter=None):
+        self.letters = {}
+        if(letter != None and letter != ""):
+            self.add(letter)
 
-    def is_list(self, elem):
-        return isinstance(elem, list)
+    def add(self, filename):
+        key = filename[0]
+        if not key in self.letters:
+            self.letters[key] = TidyTree(filename[1:])
+        else:
+            self.letters[key].add(filename[1:])
 
-    def getAllmatchs(self, pattern, list_files):
-        l = []
-        for filename in list_files:
-            if(filename[0] == pattern):
-                l += [filename[1:]]
-        return l
-
-    def remAllmatchs(self, pattern, list_files):
-        l = []
-        for filename in list_files:
-            if(filename[0] != pattern):
-                l += [filename]
-        return l
-
-
-    def tidy_list(self, list_files):
-        change = True
-        i = 0
-        while(change):
-            change = False
-            len_list = len(list_files)
-            while(i < len_list - 1):
-                if(self.is_list(list_files[i])):
-                    continue
-                gc = self.getAllmatchs(list_files[i][0], list_files)
-                if(len(gc) > self.limit):
-                    list_files = self.remAllmatchs(list_files[i][0], list_files)
-                    list_files = [gc] + list_files
-                    change = True
-                    len_list = len(list_files)
-                    i =  0
-                i = i + 1
-        for i in range(0, len(list_files) - 1):
-            if(self.is_list(list_files[i])):
-                list_files[i] = self.tidy_list(list_files[i])
-        return list_files
+    def toString(self):
+        str = ""
+        for letter in self.letters:
+            print(letter)
+            str = str + " " + letter + "(" + self.letters[letter].toString() + ") \n"
+        return str
 
 #init FOLDER path and FILES collection
 if __name__ == '__main__':
@@ -70,8 +44,10 @@ if __name__ == '__main__':
         sys.exit(1);
 
     #files = getListofFiles(folder);
-    files = [ "salut1", "salut2", "salut3", "salut4", "salut5", "salut6", "salut7", "toto1", "toto2", "toto3", "toto4", "toto5", "toto6"]
+    files = [ "salit1", "salit2", "salit3", "salit4", "salit5", "salit6", "salut1", "salut2", "salut3", "salut4", "salut5", "salut6", "salut7", "toto1", "toto2", "toto3", "toto4", "toto5", "toto6", "m1", "m2", "m3", "m4", "m5", "m6"]
 
-    tidy = TidyTree(files, folder)
-    l = tidy.tidy_list(tidy.files)
-    print(l)
+    tidy = TidyTree()
+    for i in files:
+        tidy.add(i)
+
+    print(tidy.toString())
