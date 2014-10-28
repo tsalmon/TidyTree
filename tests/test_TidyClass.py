da872f53ce33f37a860ffadc915562d74c70a766
tsalmon <th_s@hotmail.fr>
2014-10-28 10:12:32 +0100

    test_TidyClass

---
+++ tests/test_TidyClass.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+
+import sys
+
+import unittest
+
+#from helpers import TestCase
+
+#from tidytree.tidytree import TidyTree
+
+class TestTidyClass(unittest.TestCase):
+
+    #def setUp(self):
+
+    def test_sample(self):
+        print("tested")
