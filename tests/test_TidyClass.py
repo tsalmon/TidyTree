3b356eabb3237df87458a48456fd3811a7011470
tsalmon <th_s@hotmail.fr>
2014-10-29 15:04:42 +0100

    test_EqualsTidy, test optimize

--- tests/test_TidyClass.py
+++ tests/test_TidyClass.py
@@ -24,28 +24,48 @@ class TestTidyClass(unittest.TestCase):
         self.assertEquals(len(self.tidy.letters), 1)
         self.assertEquals(len(self.tidy.letters), 1)
 
-    def test_getListOfFile(self, tidy1=None, tidy2=None):
-        if(tidy1 == None):
-            folder = getListofFiles(".")
-            tidy1 = TidyTree()
-            for i in folder:
-                tidy1.add(i)
+    def test_EqualsTidy(self, tidy1=None, tidy2=None):
+        if(tidy1 != None and tidy1 != None):
+            tostring1 = tidy1.toString()
+            tostring2 = tidy2.toString()
+            self.assertEquals(tostring1, tostring2)
+        else:
+            self.assertEquals(tidy1, None)
+            self.assertEquals(tidy2, None)
 
-        if(tidy2 == None):
-            test_folder = ['.coverage',
-                            '.git',
-                            '.gitignore',
-                            'Makefile',
-                            'requirements.txt',
-                            'tests',
-                            'tidytree',
-                            'venv']
-            tidy2 = TidyTree()
-            for j in test_folder:
-                tidy2.add(j)
+    def test_getListOfFile(self):
+        folder = getListofFiles(".")
+        tidy1 = TidyTree()
+        for i in folder:
+            tidy1.add(i)
 
-        self.assertEquals(len(tidy1.letters), len(tidy2.letters))
+        test_folder = ['.coverage',
+                        '.git',
+                        '.gitignore',
+                        'Makefile',
+                        'requirements.txt',
+                        'tests',
+                        'tidytree',
+                        'venv']
+        tidy2 = TidyTree()
+        for j in test_folder:
+            tidy2.add(j)
+        self.test_EqualsTidy(tidy1, tidy2)
 
-        if(len(tidy1.letters) > 0):
-            for i in tidy1.letters:
-                self.test_getListOfFile(tidy1.letters[i], tidy2.letters[i])
+
+    def test_optimize(self):
+        test_opti = TidyTree()
+        test_opti.letters["a"] = TidyTree()
+        test_opti.letters["a"].letters["zerty"] = TidyTree()
+        test_opti.letters["a"].letters["zerty"].letters["lol"] = TidyTree()
+        test_opti.letters["a"].letters["zerty"].letters["mdr"] = TidyTree()
+        test_opti.letters["a"].letters["bc"] = TidyTree()
+        test_opti.letters["qwerty"] = TidyTree()
+
+        files = ["azertylol", "azertymdr", "qwerty", "abc"]
+        tidy = TidyTree()
+        for i in files:
+            tidy.add(i)
+        tidy.optimize()
+
+        self.test_EqualsTidy(test_opti, tidy)
